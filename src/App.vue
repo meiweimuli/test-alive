33b7fef8c869f2c4a39a49d9bace5cacb67c17b5
meiweimuli <53048974+meiweimuli@users.noreply.github.com>
2022-05-27 23:26:54 +0800

    Add files via upload

---
+++ src/App.vue
@@ -0,0 +1,9 @@
+<template>
+  <KeepAlive>
+    <TestPage />
+  </KeepAlive>
+</template>
+
+<script setup lang="ts">
+import TestPage from "./TestPage.vue";
+</script>
---
+++ src/TestPage.vue
@@ -0,0 +1,7 @@
+<template>
+  <div>{{ text }}</div>
+</template>
+
+<script setup lang="ts">
+const text = 20;
+</script>
